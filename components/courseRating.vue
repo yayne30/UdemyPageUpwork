4eba3ac7eeb060af8c84be382e58a651ca538eb5
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-11-10 01:01:36 +0300

    update courseRating

--- components/courseRating.vue
+++ components/courseRating.vue
@@ -2,13 +2,15 @@
 import { Icon } from '@iconify/vue';
 import { ref, computed } from 'vue';
 
-
-
-
 </script>
 <template>
-    <div class="w-[56vw] ml-20 px-5 py-4 mt-10 font-Zeitung border-2">
-      <h1 class="text-[20pt] font-extrabold">Frequently Bought Together</h1>
+    <div class="w-[56vw] ml-20  py-4 mt-10 font-Zeitung ">
+        <div>
+            <h1 class="text-[20pt] font-extrabold">4.4 course rating
+                54K ratings</h1>
+
+        </div>
+    
       
       
     </div>
